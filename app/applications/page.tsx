93a965316a7b680659f8a234c9fa1ca305bd1d4e
Were Samson Bruno <werefast2000@gmail.com>
2023-10-27 10:51:41 +0000

    created loan applications page

---
+++ app/applications/page.tsx
@@ -0,0 +1,49 @@
+
+import { Card, Title, Text } from '@tremor/react';
+import { queryBuilder } from '../../lib/planetscale';
+import Search from '../search';
+import LoanApplicationsTable from './table';
+
+export const dynamic = 'force-dynamic';
+
+interface LoanApplication {
+    application_id: number;
+    applicant_name: string;
+    applicant_id_number: string;
+    applicant_phone_number: string;
+    applicant_email: string;
+}
+
+export default async function ApplicationsPage({
+    searchParams
+}: {
+    searchParams: { q: string };
+}) {
+    const search = searchParams.q ?? '';
+    const loanApplications: LoanApplicationsTable[] = await queryBuilder
+        .selectFrom<LoanApplicationsTable>('loan_applications')
+        .select([
+            'application_id',
+            'applicant_name',
+            'applicant_id_number',
+            'applicant_phone_number',
+            'applicant_email',
+            'residence_address',
+            'employment_status'
+        ])
+        .where('applicant_name', 'like', `%${search}%`)
+        .execute() as LoanApplication[];
+
+    return (
+        <main className="p-4 md:p-10 mx-auto max-w-7xl">
+            <Title>Loan Applications</Title>
+            <Text>
+                A list of loan applications received from the loan application form (Absa Website).
+            </Text>
+            <Search />
+            <Card className="mt-6">
+                <LoanApplicationsTable loanApplications={loanApplications} />
+            </Card>
+        </main>
+    );
+}
